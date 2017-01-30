027dabd6db47d472cc73ae570c72f47354d07d32
Michael Knowles <michael@mapledyne.com>
2017-01-30 14:11:52 -0800

    initial bit of 'people'

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
+import json
 import random
 from slackbot.bot import Bot
 from slackbot.bot import respond_to
 
 wordpath = '/opt/words/wordlist.txt'
-
-with open(wordpath) as w:
-    wordlist = w.readlines()
-
+people_path = '/opt/opsbot/people.json'
+people = {}
 maybe = []
-for word in wordlist:
-    maybe.append(word.strip())
+
+
+def load_people():
+    with open(people_path) as data_file:
+        people = json.load(data_file)
 
 
 def generate_password():
@@ -18,13 +20,13 @@ def generate_password():
     return newpass
 
 
-@respond_to('Give me (.*)')
-def giveme(message, something):
-    message.reply('Here is {}'.format(something))
-
 @respond_to('password')
 def pass_request(message):
-    message.reply(generate_password)
+    message.reply(generate_password())
+
+@respond_to('admins')
+def pass_request(message):
+    message.reply('My admins are: {}'.format(", ".join(people[admins])))
 
 
 def main():
@@ -32,4 +34,11 @@ def main():
     bot.run()
 
 if __name__ == "__main__":
+    with open(wordpath) as w:
+        wordlist = w.readlines()
+
+    for word in wordlist:
+        maybe.append(word.strip())
+
+    load_people()
     main()
